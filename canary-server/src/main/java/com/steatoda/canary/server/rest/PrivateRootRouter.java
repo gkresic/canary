26c3332a60a4d3004a7740218964b970877896a8
gkresic <gordan.kresic@steatoda.com>
2025-07-12 18:44:02 +0200

    New pricate endpoints (for testing)

--- canary-server/src/main/java/com/steatoda/canary/server/rest/PrivateRootRouter.java
+++ canary-server/src/main/java/com/steatoda/canary/server/rest/PrivateRootRouter.java
@@ -6,11 +6,13 @@ import com.steatoda.canary.server.rest.handler.TraceHandler;
 import io.helidon.common.media.type.MediaType;
 import io.helidon.common.media.type.MediaTypes;
 import io.helidon.http.Status;
+import io.helidon.webserver.WebServer;
 import io.helidon.webserver.http.HttpRules;
 import io.helidon.webserver.http.ServerRequest;
 import io.helidon.webserver.http.ServerResponse;
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import jakarta.inject.Inject;
+import jakarta.inject.Provider;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -22,11 +24,13 @@ public class PrivateRootRouter extends RootRouter {
 		TraceHandler traceHandler,
 		LogHandler logHandler,
 		RouteNameHandler.Factory routeNameHandlerFactory,
-		PrometheusMeterRegistry prometheusMeterRegistry
+		PrometheusMeterRegistry prometheusMeterRegistry,
+		Provider<WebServer> helidonWebServerProvider
 	) {
 		super (traceHandler, logHandler);
 		this.routeNameHandlerFactory = routeNameHandlerFactory;
 		this.prometheusMeterRegistry = prometheusMeterRegistry;
+		this.helidonWebServerProvider = helidonWebServerProvider;
 	}
 
 	@Override
@@ -37,6 +41,16 @@ public class PrivateRootRouter extends RootRouter {
 		rules.get("/metrics",
 			routeNameHandlerFactory.create("metrics"),
 			this::metrics
+		);
+
+		rules.get("/healthy",
+			routeNameHandlerFactory.create("healthy"),
+			this::healthy
+		);
+
+		rules.get("/stop",
+			routeNameHandlerFactory.create("stop"),
+			this::stop
 		);
 
 	}
@@ -50,7 +64,20 @@ public class PrivateRootRouter extends RootRouter {
 		}
 	}
 
+	private void healthy(ServerRequest request, ServerResponse response) {
+		response
+			.status(Status.NO_CONTENT_204)	// in case of sickness, return Status.SERVICE_UNAVAILABLE_503
+			.send();
+	}
+
+	private void stop(ServerRequest request, ServerResponse response) {
+		response
+			.send("Bye!");
+		helidonWebServerProvider.get().stop();
+	}
+
 	private final RouteNameHandler.Factory routeNameHandlerFactory;
 	private final PrometheusMeterRegistry prometheusMeterRegistry;
+	private final Provider<WebServer> helidonWebServerProvider;
 
 }
